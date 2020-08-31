45d4361e2033c19f93dad012272271a2e9b5b249
Patrick Mullen <prmullen20@gmail.com>
2020-08-31 11:19:46 -0700

    save, dev, build working

--- client/index.js
+++ client/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { render } from 'react-dom';
 import App from './App';
 
-
+const electron = require('electron')
 
 // should grab the App component and attach to the index.html
 render(
