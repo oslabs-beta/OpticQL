07294e2dfbe309dda76b48a3ed98941b0348ea7e
bj-tseng <bonjay.tseng@gmail.com>
2020-09-03 16:51:32 -0400

    Updated functionality for front-end database inside Electron

--- production/server/server.js
+++ production/server/server.js
@@ -2,22 +2,12 @@ const path = require('path');
 const express = require('express');
 const app = express();
 
-
-const PORT = 3001;
+let PORT = 3001;
 
 app.use(express.json())
 app.use(express.urlencoded())
 
-app.use('/api/schema', (req, res) => {
-  console.log(req.body.hello)
-  // console.log(JSON.parse(req.body))
-  console.log('schema received!!!')
-  res.status(200).json({hello: 'holaTest2'})
-})
-
-
 if (process.env.NODE_ENV === 'production') {
-
   // statically serve everything in the build folder on the route '/build'. 
   // PM: So now index.html can look at build/bundle.js and find it there!!!!!!!
   app.use('/build', express.static(path.join(__dirname, '../build')));
@@ -25,9 +15,20 @@ if (process.env.NODE_ENV === 'production') {
   // serve index.html on the route '/'. 
   // PM: We need to send index.html first, and then index html runs the script that loads the webpack data from build/bundle.js
   app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, '../client/index.html'));
+    return res.sendFile(path.join(__dirname, '../client/index.html'));
   });
-
 }
+
+app.use('/api/schema', (req, res) => {
+  console.log(req.body.hello)
+  // console.log(JSON.parse(req.body))
+  console.log('schema received!!!')
+  return res.status(200).json({hello: 'holaTest2'})
+})
+
+// For development mode ONLY --- to prevent error codes with application seeking 2nd instance of server file
+app.use('/server/server.js', (req, res) => {
+  return res.sendFile(path.join(__dirname, './server2.js'));
+})
 
 app.listen(PORT, () => console.log(`Listening on PORT: ${PORT}`));
