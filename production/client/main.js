5aa8b94deabefeb5f09d6974c790a0d39a43a92b
Rebecca Miller <beemills@gmail.com>
2020-09-22 12:17:25 -0400

    removed iconpath from main.js

--- production/client/main.js
+++ production/client/main.js
@@ -1,13 +1,11 @@
 const { app, BrowserWindow } = require('electron');
 const path = require('path');
 
-const iconPath = path.join(__dirname, "/client/assets/icon.png");
 function createWindow () {
 	// Create the browser window.
 	const win = new BrowserWindow({
 		width: 1280,
 		height: 720,
-		icon: iconPath,
 		webPreferences: {
 			nodeIntegration: true,
 			nativeWindowOpen: true,
